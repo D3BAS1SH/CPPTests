085855cf2bdaac9c0d8682f8073ab377f9de992a
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-05-03 09:41:41 +0530

    LCS PROGRAM COMPLETION.

--- Tests/LCS.cpp
+++ Tests/LCS.cpp
@@ -4,6 +4,33 @@
 
 using namespace std;
 
+int printLCS(string X, vector<vector<char>> CMAT,int M,int N) {
+	//cout << X << ":(" << CMAT.size() << "X" << CMAT[0].size() << ")-" << M << ":" << N << endl;
+	if (M == 0 || N == 0) {
+		return 0;
+	}
+
+	
+
+	if (CMAT[M][N] == 'D') {
+		//cout << "GOING x" << endl;
+		printLCS(X, CMAT, M - 1, N - 1);
+		//cout << "GOING OUT x" << endl;
+		cout << X[M-1];
+	}
+	else if (CMAT[M][N] == 'U') {
+		//cout << "GOING xx" << endl;
+		printLCS(X, CMAT, M - 1, N);
+		//cout << "GOING OUT xx" << endl;
+	}
+	else if (CMAT[M][N] == 'L') {
+		//cout << "GOING xxx" << endl;
+		printLCS(X, CMAT, M, N - 1);
+		//cout << "GOING OUT xxx" << endl;
+	}
+
+}
+
 int main() {
 	string MAIN_STR, AUX_STR;
 
@@ -33,13 +60,23 @@ int main() {
 				CHAR_MAT[i][j] = 'U';
 				INT_MAT[i][j] = INT_MAT[i - 1][j];
 			}
-			else if (INT_MAT[i][j] == INT_MAT[i][j - 1]) {
+			else{
 				CHAR_MAT[i][j] = 'L';
+				INT_MAT[i][j] = INT_MAT[i][j - 1];
 			}
+			//cout << INT_MAT[i][j] << ":" << CHAR_MAT[i][j] << "\t";
+		}
+		//cout << endl;
+	}
+
+	for (int i = 0; i < INT_MAT.size(); i++) {
+		for (int j = 0; j < INT_MAT[0].size(); j++) {
 			cout << INT_MAT[i][j] << ":" << CHAR_MAT[i][j] << "\t";
 		}
 		cout << endl;
 	}
 
-	cout << MAIN_STR << "," << AUX_STR << endl;
+	printLCS(MAIN_STR,CHAR_MAT,CHAR_MAT.size()-1,CHAR_MAT[0].size()-1);
+
+	//cout << MAIN_STR << "," << AUX_STR << endl;
 }
